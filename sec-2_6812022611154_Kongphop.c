cd3f2ceadca6a2a715d4d352e6e35cd0bf5eb17c
Kongphop6812022611154 <s6812022611154@email.kmutnb.ac.th>
2025-10-07 15:23:53 +0700

    Add files via upload

---
+++ sec-2_6812022611154_Kongphop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    char type;
+
+    printf("***Transaction fee Calculator***\n");
+    printf("Enter number of transaction (3-12): ");
+    scanf("%d");
+    printf("Enter Channel Type for TXN 1 (O, B, A): ");
+    scanf("%d");
+    return 0;
+}
